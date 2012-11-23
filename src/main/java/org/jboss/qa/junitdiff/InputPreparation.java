f30cc830c0335f5b815f890ae1a341b7eb7ae7a3
OndraZizka <zizka@seznam.cz>
2012-11-23 18:12:34 +0100

    Check for binary files.

--- src/main/java/org/jboss/qa/junitdiff/InputPreparation.java
+++ src/main/java/org/jboss/qa/junitdiff/InputPreparation.java
@@ -256,16 +256,26 @@ public class InputPreparation
     private static boolean isBinaryFile( File f ) throws FileNotFoundException, IOException {
         FileInputStream in = new FileInputStream(f);
         int size = in.available();
-        if(size > 1000) size = 1000;
+        if(size > 1024) size = 1024;
         byte[] data = new byte[size];
         in.read(data);
         in.close();
         
+        int ascii = 0;
+        int other = 0;
+        
         for( int i = 0; i < data.length; i++ ) {
-            if( data[i] < 0x09 )
-                return true;
+            byte b = data[i];
+            if( b < 0x09 ) return true;
+            
+            if( b == 0x09 || b == 0x0A || b == 0x0C || b == 0x0D ) ascii++;
+            else if( b > 0x20  &&  b < 0x7E ) ascii++;
+            else other++;
         }
-        return false;
+        
+        if( other == 0 ) return false;
+        
+        return (ascii + other) * 100 / other > 95;
     }
 
 }// class
