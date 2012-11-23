8dfc93c8350cfa5dc752d0d3d7b5eb54abf1257a
OndraZizka <zizka@seznam.cz>
2012-11-23 15:33:31 +0100

    Format

--- src/main/java/org/jboss/qa/junitdiff/InputPreparation.java
+++ src/main/java/org/jboss/qa/junitdiff/InputPreparation.java
@@ -1,6 +1,4 @@
-
 package org.jboss.qa.junitdiff;
-
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -29,46 +27,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 /**
  *
  * @author Ondrej Zizka
  */
 public class InputPreparation
 {
-		private static final Logger log = LoggerFactory.getLogger( InputPreparation.class );
+    private static final Logger log = LoggerFactory.getLogger( InputPreparation.class );
 
 
 
+    /**
+     *  Takes a list of paths which may be directories, .txt lists of paths, or JUnit XML reports,
+     *  and replaces directories with TEST-*.xml's inside,
+     *  and expands .txt lists of paths.
+     */
+    static List<File> preprocessPaths( List<File> reportFiles ) {
+        List<File> expandedPaths = new LinkedList();
 
-		/**
-		 *  Takes a list of paths which may be directories, .txt lists of paths, or JUnit XML reports,
-		 *  and replaces directories with TEST-*.xml's inside,
-		 *  and expands .txt lists of paths.
-		 */
-		static List<File> preprocessPaths( List<File> reportFiles ) {
-				List<File> expandedPaths = new LinkedList();
+        for( File path : reportFiles ) {
 
-				for( File path : reportFiles ) {
+            if( path.isFile() ){
+                if( path.getName().endsWith(".xml") ){
+                    expandedPaths.add( path );
+                }
+                else {
+                    expandedPaths.addAll( readListOfPaths( path ) );
+                }
+            }
 
-						if( path.isFile() ){
-								if( path.getName().endsWith(".xml") ){
-										expandedPaths.add( path );
-								}
-								else {
-										expandedPaths.addAll( readListOfPaths( path ) );
-								}
-						}
+            if( path.isDirectory() ){
+                expandedPaths.addAll( scanDirForJUnitReports( path ) );
+                continue;
+            }
 
-						if( path.isDirectory() ){
-								expandedPaths.addAll( scanDirForJUnitReports( path ) );
-								continue;
-						}
+        }
 
-				}
-
-				return expandedPaths;
-		}
+        return expandedPaths;
+    }
 
 
 
@@ -78,69 +74,69 @@ public class InputPreparation
      * Handle URLs: If the path starts with http://, it downlads the file and unzips if it ends with .zip.
      * Paths will be replaced in-place in the array.
      */
-    static void handleURLs( String[] paths )
-    {
-      for( int i = 0; i < paths.length; i++ ) {
-        String path = paths[i];
-        if( path == null ) continue;
+    static void handleURLs( String[] paths ) {
+        for( int i = 0; i < paths.length; i++ ) {
+            String path = paths[i];
+            if( path == null ) {
+                continue;
+            }
 
-        // Only replace URL's.
-        if( ! path.startsWith("http://") )   continue;
+            // Only replace URL's.
+            if( !path.startsWith( "http://" ) ) {
+                continue;
+            }
 
-        try{
-          File resultDir;
-          if( path.endsWith(".zip"))
-            resultDir = downloadZipAndExtractToTempDir( path );
-          else if( path.endsWith(".xml")){
-            resultDir = downloadUrlToTempFile( path );
-          }
-          else {
-            log.warn("  URL is not .zip nor .xml - skipping: "+path);
-            continue;
-          }
-          paths[i] = resultDir.getPath();
+            try {
+                File resultDir;
+                if( path.endsWith( ".zip" ) ) {
+                    resultDir = downloadZipAndExtractToTempDir( path );
+                } else if( path.endsWith( ".xml" ) ) {
+                    resultDir = downloadUrlToTempFile( path );
+                } else {
+                    log.warn( "  URL is not .zip nor .xml - skipping: " + path );
+                    continue;
+                }
+                paths[i] = resultDir.getPath();
+            } catch( IOException ex ) {
+                log.warn( "  Error when processing URL " + path + ": " + ex.getMessage(), ex );
+            }
+
         }
-        catch( IOException ex ){
-          log.warn("  Error when processing URL "+path+": "+ex.getMessage(), ex);
-        }
-
-      }
     }
 
 
     /**
      * 
      */
-    private static File downloadZipAndExtractToTempDir( String urlStr ) throws MalformedURLException, IOException
-    {
-      // Download
-      File tmpFile = downloadUrlToTempFile( urlStr );
+    private static File downloadZipAndExtractToTempDir( String urlStr ) throws MalformedURLException, IOException {
+        // Download
+        File tmpFile = downloadUrlToTempFile( urlStr );
 
-      // Unzip & return the tmp dir.
-      File dirWithZipContent = unzipFileToTempDir( tmpFile );
-      tmpFile.delete();
-      return dirWithZipContent;
+        // Unzip & return the tmp dir.
+        File dirWithZipContent = unzipFileToTempDir( tmpFile );
+        tmpFile.delete();
+        return dirWithZipContent;
     }
 
     private static File downloadUrlToTempFile( String urlStr ) throws MalformedURLException, IOException {
-      URL url = new URL( urlStr );
+        URL url = new URL( urlStr );
 
-			// Create the directory.
-			String path = url.getPath();
-			path = StringUtils.strip(path, "\\/");
-			path = path.replace('*', '-');
-			path = path.replace('?', '-');
-			File destZipFile = new File( path );
-			destZipFile.getParentFile().mkdirs();
+        // Create the directory.
+        String path = url.getPath();
+        path = StringUtils.strip(path, "\\/");
+        path = path.replace('*', '-');
+        path = path.replace('?', '-');
+        File destZipFile = new File( path );
+        destZipFile.getParentFile().mkdirs();
 
 
-      ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-      //File tmp = File.createTempFile( "JUnitDiff-tmp-", ".zip" );
-      FileOutputStream fos = new FileOutputStream( destZipFile );
-      fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-      rbc.close();
-      fos.close();
-      return destZipFile;
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        //File tmp = File.createTempFile( "JUnitDiff-tmp-", ".zip" );
+        FileOutputStream fos = new FileOutputStream( destZipFile );
+        fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+        rbc.close();
+        fos.close();
+        return destZipFile;
     }
 
 
@@ -148,109 +144,96 @@ public class InputPreparation
 		/**
 		 *  Reads a list of paths from a text file, one per line. Not recursive.
 		 */
-		private static List<File> readListOfPaths( File path ) {
+    private static List<File> readListOfPaths( File path ) {
 
-				try {
-						List<String> lines = FileUtils.readLines(path);
-						List<File> paths = new ArrayList( lines.size() );
+        try {
+            List<String> lines = FileUtils.readLines( path );
+            List<File> paths = new ArrayList( lines.size() );
 
-						for (String line : lines) {
-								File f = new File(line);
-								if( ! f.exists() )
-										log.warn("  Does not exist: "+f.getPath());
-								else if( ! f.isFile() )
-										log.warn("  Not a regular file: "+f.getPath());
-								else
-										paths.add(f);
-						}
+            for( String line : lines ) {
+                File f = new File( line );
+                if( !f.exists() ) {
+                    log.warn( "  Does not exist: " + f.getPath() );
+                } else if( !f.isFile() ) {
+                    log.warn( "  Not a regular file: " + f.getPath() );
+                } else {
+                    paths.add( f );
+                }
+            }
 
-						return paths;
-				}
-				catch (IOException ex) {
-						log.warn("Error reading "+path.getPath()+" : "+ex.getMessage());
-						return Collections.EMPTY_LIST;
-				}
+            return paths;
+        } catch( IOException ex ) {
+            log.warn( "Error reading " + path.getPath() + " : " + ex.getMessage() );
+            return Collections.EMPTY_LIST;
+        }
 
-		}
+    }
 
 
 		/**
-		 * Scans a directory for JUnit test reports.
-		 */
-		private static List<File> scanDirForJUnitReports( final File path )
-		{
-				IOFileFilter filter = FileFilterUtils.or(
-						FileFilterUtils.directoryFileFilter(),
-						FileFilterUtils.and(
-								// Try to filter out as much as possible before opening the file below.
-								FileFilterUtils.notFileFilter(
-										FileFilterUtils.or(
-												FileFilterUtils.suffixFileFilter(".jar", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".html", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".zip", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".txt", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".log", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".class", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".java", IOCase.INSENSITIVE),
-												FileFilterUtils.suffixFileFilter(".", IOCase.INSENSITIVE)
-										)
-								),
-								FileFilterUtils.or(
-										// Perhaps make this an option - some other filters like content-based etc.
-										FileFilterUtils.suffixFileFilter(".xml"),
-										FileFilterUtils.prefixFileFilter("TEST"),
-										FileFilterUtils.magicNumberFileFilter("<?xml")
-								)
-						)// and
-						// Maybe we could simply scan for TEST-*.xml names.
-				);
+     * Scans a directory for JUnit test reports.
+     */
+    private static List<File> scanDirForJUnitReports( final File path ) {
+        IOFileFilter filter = FileFilterUtils.or(
+                FileFilterUtils.directoryFileFilter(),
+                FileFilterUtils.and(
+                // Try to filter out as much as possible before opening the file below.
+                FileFilterUtils.notFileFilter(
+                FileFilterUtils.or(
+                FileFilterUtils.suffixFileFilter( ".jar", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".html", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".zip", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".txt", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".log", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".class", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".java", IOCase.INSENSITIVE ),
+                FileFilterUtils.suffixFileFilter( ".", IOCase.INSENSITIVE ) ) ),
+                FileFilterUtils.or(
+                // Perhaps make this an option - some other filters like content-based etc.
+                FileFilterUtils.suffixFileFilter( ".xml" ),
+                FileFilterUtils.prefixFileFilter( "TEST" ),
+                FileFilterUtils.magicNumberFileFilter( "<?xml" ) ) )// and
+                // Maybe we could simply scan for TEST-*.xml names.
+                );
 
-				IOFileFilter rigidFilter = FileFilterUtils.or(
-						FileFilterUtils.directoryFileFilter(),
-						FileFilterUtils.and(
-										// Perhaps make this an option - some other filters like content-based etc.
-										FileFilterUtils.suffixFileFilter(".xml"),
-										FileFilterUtils.prefixFileFilter("TEST")
-										//FileFilterUtils.magicNumberFileFilter("<?xml")
-						)// and
-						// Maybe we could simply scan for TEST-*.xml names.
-				);
+        IOFileFilter rigidFilter = FileFilterUtils.or(
+                FileFilterUtils.directoryFileFilter(),
+                FileFilterUtils.and(
+                // Perhaps make this an option - some other filters like content-based etc.
+                FileFilterUtils.suffixFileFilter( ".xml" ),
+                FileFilterUtils.prefixFileFilter( "TEST" ) //FileFilterUtils.magicNumberFileFilter("<?xml")
+                )// and
+                // Maybe we could simply scan for TEST-*.xml names.
+                );
 
 
-				// Walk trough the dir tree...
-				try {
-						LinkedList resultList  = new LinkedList();
-						new DirectoryWalker( rigidFilter, -1 )
-						{
+        // Walk trough the dir tree...
+        try {
+            LinkedList resultList = new LinkedList();
+            new DirectoryWalker( rigidFilter, -1 ) {
+                @Override
+                protected void handleFile( File file, int depth, Collection results ) throws IOException {
+                    //		log.info("  Handling file: " + file.getPath());///
+                    results.add( file );
+                }
 
-								@Override
-								protected void handleFile( File file, int depth, Collection results ) throws IOException {
-									//		log.info("  Handling file: " + file.getPath());///
-									results.add( file );
-								}
+                @Override
+                protected boolean handleDirectory( File directory, int depth, Collection results ) throws IOException {
+                    //log.info( "  Handling dir: "+directory.getPath() );
+                    return true;
+                }
 
+                public void doWalk( LinkedList list ) throws IOException {
+                    this.walk( path, list );
+                }
+            }.doWalk( resultList );
 
-								@Override
-								protected boolean handleDirectory( File directory, int depth, Collection results ) throws IOException {
-									//log.info( "  Handling dir: "+directory.getPath() );
-									return true;
-								}
+            return resultList;
+        } catch( IOException ex ) {
+            return Collections.EMPTY_LIST;
+        }
 
-
-								public void doWalk( LinkedList list ) throws IOException{
-									this.walk( path, list );
-								}
-
-						}.doWalk( resultList );
-
-						return resultList;
-				}
-				catch ( IOException ex ){
-						return Collections.EMPTY_LIST;
-				}
-
-		}// scanDirForJUnitReports()
-
+    }// scanDirForJUnitReports()
 
 
 
@@ -311,16 +294,13 @@ public class InputPreparation
 				is.close();
 			}
 		}
-      catch(Exception ex) {
-         log.error(" Error when unzipping "+file.getPath()+": "+ex.getMessage() );
-         tmpDir.delete();
-      }
+        catch( Exception ex ) {
+            log.error( " Error when unzipping " + file.getPath() + ": " + ex.getMessage() );
+            tmpDir.delete();
+        }
 
-      return tmpDir;
+        return tmpDir;
     }
-
-
-
 
 
 }// class
