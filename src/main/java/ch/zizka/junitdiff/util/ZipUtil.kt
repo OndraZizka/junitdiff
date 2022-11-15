2df3c1a09c56a1a9ddaaa7269384f5aeec59c417
Ondrej Zizka <ondrej.zizka@dacadoo.com>
2022-11-15 22:27:42 +0100

    Cleanup

--- src/main/java/ch/zizka/junitdiff/util/ZipUtil.kt
+++ src/main/java/ch/zizka/junitdiff/util/ZipUtil.kt
@@ -4,7 +4,8 @@ import org.apache.commons.io.FileUtils
 import org.apache.commons.io.filefilter.FileFilterUtils
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
-import java.io.*
+import java.io.File
+import java.io.FileFilter
 import java.nio.file.Files
 import java.util.*
 import java.util.zip.ZipEntry
@@ -19,10 +20,6 @@ object ZipUtil {
     private val log = LoggerFactory.getLogger(ZipUtil::class.java)
     private const val BUFFER_SIZE = 1024 * 32
 
-    /**
-     * Unzips a file to a temporary dir.
-     */
-    @Throws(IOException::class)
     fun unzipFileToTempDir(zipFile: File?, mode: OverwriteMode?): File {
 
         // Try to keep the original path in the new path for the group naming purposes.
@@ -47,7 +44,6 @@ object ZipUtil {
      * If ONLY_NEW and the dir exists, returns false and does nothing.
      * @return true if the zip was unzipped to the given dir.
      */
-    @Throws(IOException::class)
     fun unzipFileToDir(zip: File?, intoDir: File, mode: OverwriteMode): Boolean {
         if (intoDir.exists()) {
             if (mode == OverwriteMode.ONLY_NEW) {
@@ -59,14 +55,10 @@ object ZipUtil {
         unzipFileToDir(zip, intoDir)
         return true
     }
-    /**
-     * Unzip method which can filter files to extract.
-     */
-    /**
-     * Unzip method.
-     */
+
+
+    /** Unzip method which can filter files to extract. */
     @JvmOverloads
-    @Throws(IOException::class)
     fun unzipFileToDir(zipFile: File?, intoDir: File?, fileFilter: FileFilter? = null as FileFilter?, mode: OverwriteMode? = OverwriteMode.WRITE_INTO) {
         val zip = ZipFile(zipFile)
         val entries = zip.entries() as Enumeration<ZipEntry>
@@ -83,14 +75,14 @@ object ZipUtil {
                     f.parentFile.mkdirs()
                     f.createNewFile()
                 }
-                val `is` = zip.getInputStream(entry)
+                val iStream = zip.getInputStream(entry)
                 val os = Files.newOutputStream(f.toPath())
                 var r: Int
-                while (`is`.read(buf).also { r = it } != -1) {
+                while (iStream.read(buf).also { r = it } != -1) {
                     os.write(buf, 0, r)
                 }
                 os.close()
-                `is`.close()
+                iStream.close()
             }
         } catch (ex: Exception) {
             log.error(" Error when unzipping " + zipFile!!.path + ": " + ex.message)
