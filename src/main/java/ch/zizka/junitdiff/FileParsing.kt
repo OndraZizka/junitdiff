5dddab6476eea8c4040182b38e44c10fef57b196
Ondrej Zizka <ondrej.zizka@dacadoo.com>
2022-11-15 17:27:56 +0100

    Trying to switch to JDOM 2

--- src/main/java/ch/zizka/junitdiff/FileParsing.kt
+++ src/main/java/ch/zizka/junitdiff/FileParsing.kt
@@ -3,11 +3,11 @@ package ch.zizka.junitdiff
 import ch.zizka.junitdiff.ex.JUnitDiffException
 import ch.zizka.junitdiff.model.*
 import org.apache.commons.io.FileUtils
-import org.jdom.Document
-import org.jdom.Element
-import org.jdom.JDOMException
-import org.jdom.input.SAXBuilder
-import org.jdom.xpath.XPath
+import org.jdom2.Document
+import org.jdom2.Element
+import org.jdom2.JDOMException
+import org.jdom2.input.SAXBuilder
+import org.jdom2.xpath.XPath
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileInputStream
